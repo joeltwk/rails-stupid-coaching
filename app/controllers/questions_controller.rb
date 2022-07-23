e18eaa4e485e300cf752556aef21dfda851f3f4f
Joel Tang <joeltwk@gmail.com>
2022-07-23 13:40:11 +0800

    stupid coaching

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+# Questions Controller
+class QuestionsController < ApplicationController
+  def ask; end
+
+  def answer
+    question = params[:question]
+
+    @answer = if question.downcase == 'i am going to work'
+                'Great!'
+              elsif question.downcase.end_with?('?')
+                'Silly question, get dressed and go to work!'
+              else
+                "I don't care, get dressed and go to work!"
+              end
+  end
+end
